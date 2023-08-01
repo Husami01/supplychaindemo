94a94037171bbcf8befff003a2a4ac304b393bb2
Husami01 <138901882+Husami01@users.noreply.github.com>
2023-08-01 14:05:20 -0500

    json obj

--- app.py
+++ app.py
@@ -60,13 +60,21 @@ def remove_from_cart():
 def process_cart():
     flow_url = "https://prod-33.westus.logic.azure.com:443/workflows/0ade75fc9afa4a60886ae76f69f1c5d4/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=IoWUjcMxm5sTGbINhBid9zhgco_RLHPZ8sm8d0JhA68"
     data = request.get_json()
-    global fName
+    global fName, lName, address, cartTotal
     fName = data.get('fname')
-    global lName 
     lName = data.get('lname')
-    global address
     address = data.get('addr')
-    response = requests.post(flow_url, json={"First Name": fName, "Price":cartTotal})
+    state = data.get('state')
+    order_data = {
+        "First Name": fName, 
+        "Last Name": lName, 
+        "Address": address, 
+        "State": state,
+        "Price": cartTotal
+    }
+    order_data.update(orderItemQuantities)
+    print(order_data)
+    response = requests.post(flow_url, json=order_data)
     return 'Order Placed'
     
 @app.route('/updateTotal', methods=['POST'])
