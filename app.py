044f08b4577c89003d0214c11aeab609d5684a22
Husami01 <138901882+Husami01@users.noreply.github.com>
2023-08-03 10:58:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import json
 import requests
 
 app = Flask(__name__)
+app.secret_key = 'some-secret-key' # Replace with a strong secret key
 
-orderItemQuantities = {
-    'EL0001': 0,
-    'EL0002': 0,
-    'EL0003': 0,
-    'EL0004': 0,
-    'EL0005': 0,
-    'EL0006': 0,
-    'EL0007': 0,
-    'EL0008': 0,
-    'EL0009': 0,
-    'EL0010': 0,
-    'EL0011': 0,
-    'EL0012': 0,
-    'EL0013': 0,
-    'EL0014': 0,
-    'EL0015': 0,
-    'EL0016': 0,
-    'EL0017': 0,
-    'EL0018': 0,
-    'EL0019': 0,
-    'EL0020': 0
-}
-
-fName = ""
-lName = ""
-address = ""
-cartTotal = 0
 @app.route("/")
 def index():
-	return render_template("index.html")
+    session['orderItemQuantities'] = {f'EL{i:04d}': 0 for i in range(1, 21)}
+    session['cartTotal'] = 0
+    return render_template("index.html")
 
 @app.route("/checkout", methods=['GET', 'POST'])
 def checkout():
-    global cartTotal
-    rounded_total = round(cartTotal, 2)
+    rounded_total = round(session['cartTotal'], 2)
     return render_template('checkout.html', cartTotal=rounded_total)
     
 @app.route('/add_to_cart', methods=['POST'])
@@ -46,7 +21,10 @@ def add_to_cart():
     data = request.get_json()
     item = data.get('item')
     quantity = data.get('quantity')
-    orderItemQuantities[item] += int(quantity)
+    if 'orderItemQuantities' not in session:
+        session['orderItemQuantities'] = {f'EL{i:04d}': 0 for i in range(1, 21)}
+    session['orderItemQuantities'][item] += int(quantity)
+    session.modified = True  # Ensure that the session is marked as modified
     return "Cart updated", 200
 
 @app.route('/remove_from_cart', methods=['POST'])
@@ -54,38 +32,48 @@ def remove_from_cart():
     data = request.get_json()
     item = data.get('item')
     quantity = data.get('quantity')
-    orderItemQuantities[item] = max(orderItemQuantities[item] - int(quantity), 0)
-    return "Cart updated", 200
+    # Check if the item exists in the session and the quantity is greater than 0
+    if 'orderItemQuantities' in session and item in session['orderItemQuantities']:
+        session['orderItemQuantities'][item] = max(session['orderItemQuantities'][item] - int(quantity), 0)
+        session.modified = True  # Ensure that the session is marked as modified
+        return "Cart updated", 200
+    else:
+        return "Item not found in cart", 400
+
 
 @app.route('/process_order', methods=['POST'])
 def process_cart():
     flow_url = "https://prod-33.westus.logic.azure.com:443/workflows/0ade75fc9afa4a60886ae76f69f1c5d4/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=IoWUjcMxm5sTGbINhBid9zhgco_RLHPZ8sm8d0JhA68"
     data = request.get_json()
-    global fName, lName, address, cartTotal
     fName = data.get('fname')
     lName = data.get('lname')
     address = data.get('addr')
     state = data.get('state')
+    cartTotal = session['cartTotal']
     order_data = {
-        "First Name": fName, 
-        "Last Name": lName, 
-        "Address": address, 
+        "First Name": fName,
+        "Last Name": lName,
+        "Address": address,
         "State": state,
         "Price": cartTotal
     }
-    order_data.update(orderItemQuantities)
-    count_non_zero_values = sum(value != 0 for value in orderItemQuantities.values())
+    order_data.update(session['orderItemQuantities'])
+    count_non_zero_values = sum(value != 0 for value in session['orderItemQuantities'].values())
     diffProducts = {'diffProducts': count_non_zero_values}
     order_data.update(diffProducts)
     response = requests.post(flow_url, json=order_data)
+
+    # Optionally, you could reset the cart after processing the order:
+    session['orderItemQuantities'] = {f'EL{i:04d}': 0 for i in range(1, 21)}
+    session['cartTotal'] = 0
     return 'Order Placed'
-    
+
+
 @app.route('/updateTotal', methods=['POST'])
 def updateTotal():
-    global cartTotal
     data = request.get_json()
-    cartTotal = data.get('total')
+    session['cartTotal'] = data.get('total')
     return "Total updated successfully"
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
